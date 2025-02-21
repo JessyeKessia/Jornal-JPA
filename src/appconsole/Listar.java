a5482c64f4c60f4c0baf282add90012d0c2af5fa
jessyekessia <jessyekessia16@gmail.com>
2025-02-21 10:02:57 -0300

    Terminando o listar

--- src/appconsole/Listar.java
+++ src/appconsole/Listar.java
@@ -1,42 +1,47 @@
 package appconsole;
-/**********************************
- * IFPB - SI
- * Persistencia de Objetos
- * Prof. Fausto Ayres
- **********************************/
 
-
-
-import modelo.Noticia;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import modelo.Assunto;
+import modelo.Noticia;
 import regras_negocio.Fachada;
+import java.util.List;
 
 public class Listar {
+    protected EntityManager manager;
 
-	public Listar(){
-		try {
-			Fachada.inicializar();
+    public Listar() {
+        try {
+            manager = Util.conectarBanco();
+            String jpql;
 
-			System.out.println("*** Listagem de Noticias:");
-			for(Noticia n : Fachada.listarNoticias())		
-				System.out.println(n);
+            
 
+            System.out.println("*** Listagem de Notícias:");
+            jpql = "SELECT n FROM Noticia n";
+            TypedQuery<Noticia> queryNoticias = manager.createQuery(jpql, Noticia.class);
+            List<Noticia> noticias = queryNoticias.getResultList();
+            for (Noticia n : noticias) {
+                System.out.println(n);
+            }
 
-			System.out.println("\n*** Listagem de Assuntos:");
-			for(Assunto a : Fachada.listarAssuntos())
-				System.out.println(a);
+       
+            System.out.println("\n*** Listagem de Assuntos:");
+            jpql = "SELECT a FROM Assunto a";
+            TypedQuery<Assunto> queryAssuntos = manager.createQuery(jpql, Assunto.class);
+            List<Assunto> assuntos = queryAssuntos.getResultList();
+            for (Assunto a : assuntos) {
+                System.out.println(a);
+            }
 
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		Fachada.finalizar();
-	}
+        } catch (Exception e) {
+            System.out.println("Erro ao listar dados: " + e.getMessage());
+        } finally {
+            Util.fecharBanco();
+        }
+    }
 
-
-	//=================================================
-	public static void main(String[] args) {
-		new Listar();
-	}
+    public static void main(String[] args) {
+        new Listar();
+    }
 }
-
